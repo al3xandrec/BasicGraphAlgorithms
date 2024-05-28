bbe39083c34dc042060ddeb4a39743c23cce0f51
Xan <al3xandrec@hotmail.com>
2024-05-28 15:14:08 -0300

    Update main.py

--- Python/main.py
+++ Python/main.py
@@ -1,4 +1,4 @@
-from BFS import BFS
+#from BFS import BFS
 from FordFulkerson import FordFulkerson
 
 #def BFSReadTxt():
@@ -45,6 +45,9 @@ def FFReadTxt():
 def main():
     n, adjMatrix = FFReadTxt()
     flowMatrix = FordFulkerson(n, adjMatrix, 0, 3)
-    print(flowMatrix)
+    for i in range(n):
+        for j in range(n):
+            print(f"{flowMatrix[i][j]:8.2f}", end=" ")
+        print("")
 
 main()
