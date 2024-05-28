eee4f14723b50ee5da5acb207ca6592fa968db44
Xan <al3xandrec@hotmail.com>
2024-05-28 00:29:45 -0300

    temp commit

--- libs/Graphs.h
+++ libs/Graphs.h
@@ -2,6 +2,30 @@
 
 #define XAN_GRAPHS_H
 
+class Edge {
+	private:
+		int head, tail;
+		float edgeWeight;
+	public:
+		void SetEdge(int h, int t, float w) {
+				head = h;
+				tail = t;
+				edgeWeight = w;
+		}
+		int GetEdgeEndpoints(int ht) { // 1 for head, 2 for tails
+			if (ht == 1) return head;
+			if (ht == 2) return tail;
+		}
+		
+		void SetEdgeWeight(float w) {
+			edgeWeight = w;
+		}
+		
+		float GetEdgeWeight(void) {
+			return weight;
+		}
+};
+
 class SimpleGraph {
 	private:
 		int numberOfVertices, numberOfEdges; // order and size
