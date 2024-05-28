10c34f681f76d2da1a37ae846e2f675800e436fc
Xan <al3xandrec@hotmail.com>
2024-05-28 15:33:55 -0300

    .

---
+++ Python/ReadTxt.py
@@ -0,0 +1,39 @@
+#def BFSReadTxt():
+#    filename = "input_txts/BFS_adjMatrix.txt"
+#    adjMatrix = []
+#    with open(filename, 'r') as file:
+#        lines = file.readlines()
+#        n = int(lines[0])
+#        for i in range(1, n+1):
+#            adjMatrix.append(lines[i].split())
+#        weights = lines[n+1].split()
+#    for i in range(n):
+#        adjMatrix[i] = [int(adjMatrix[i][j]) for j in range(n)]
+#    weights = [int(weights[i]) for i in range(n)]
+#    return n, adjMatrix, weights
+
+
+def BFSReadTxt():
+    filename = "input_txts/BFS_adjMatrix.txt"
+    adjMatrix = []
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        n = int(lines[0])
+        for i in range(1, n+1):
+            adjMatrix.append(lines[i].split())
+    for i in range(n):
+        adjMatrix[i] = [int(adjMatrix[i][j]) for j in range(n)]
+    return n, adjMatrix
+
+
+def FFReadTxt():
+    filename = "input_txts/FF_adjMatrix.txt"
+    adjMatrix = []
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        n = int(lines[0])
+        for i in range(1, n+1):
+            adjMatrix.append(lines[i].split())
+    for i in range(n):
+        adjMatrix[i] = [float(adjMatrix[i][j]) for j in range(n)]
+    return n, adjMatrix
--- Python/main.py
+++ Python/main.py
@@ -1,45 +1,6 @@
 #from BFS import BFS
 from FordFulkerson import FordFulkerson
-
-#def BFSReadTxt():
-#    filename = "input_txts/BFS_adjMatrix.txt"
-#    adjMatrix = []
-#    with open(filename, 'r') as file:
-#        lines = file.readlines()
-#        n = int(lines[0])
-#        for i in range(1, n+1):
-#            adjMatrix.append(lines[i].split())
-#        weights = lines[n+1].split()
-#    for i in range(n):
-#        adjMatrix[i] = [int(adjMatrix[i][j]) for j in range(n)]
-#    weights = [int(weights[i]) for i in range(n)]
-#    return n, adjMatrix, weights
-
-
-def BFSReadTxt():
-    filename = "input_txts/BFS_adjMatrix.txt"
-    adjMatrix = []
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-        n = int(lines[0])
-        for i in range(1, n+1):
-            adjMatrix.append(lines[i].split())
-    for i in range(n):
-        adjMatrix[i] = [int(adjMatrix[i][j]) for j in range(n)]
-    return n, adjMatrix
-
-
-def FFReadTxt():
-    filename = "input_txts/FF_adjMatrix.txt"
-    adjMatrix = []
-    with open(filename, 'r') as file:
-        lines = file.readlines()
-        n = int(lines[0])
-        for i in range(1, n+1):
-            adjMatrix.append(lines[i].split())
-    for i in range(n):
-        adjMatrix[i] = [float(adjMatrix[i][j]) for j in range(n)]
-    return n, adjMatrix
+from ReadTxt import *
 
 
 def main():
