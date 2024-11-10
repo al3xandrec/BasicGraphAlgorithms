a237b06fc3dfcc8377cf32791273f9f9c12b541f
Alexandre Cezar (Xan) <101900699+al3xandrec@users.noreply.github.com>
2024-11-10 11:51:15 -0300

    small fixes

--- C++/libs/Graphs.h
+++ C++/libs/Graphs.h
@@ -1,18 +1,64 @@
-// Graph Class Designed by XANADE
+// C++ Graph Class Designed by XANADE
 
+#ifndef XAN_GRAPHS_H
 #define XAN_GRAPHS_H
+
+#include <set>
+
+// Declarations
+
+class Vertex;
+class Edge;
+class Simplegraph;
+
+
+// Implementations
+
+class Vertex {
+	private:
+		int vertexName;
+		Edge *incidenceList;
+	
+	public:
+		Vertex(int name) {
+			this->vertexName = name;
+			this->incidenceList = NULL;
+		}
+		~Vertex() {
+			free(this->incidenceList);
+		}
+
+		int GetVertexName() {
+			return this->vertexName;
+		}
+
+		bool operator==(Vertex v) {
+			if (vertexName == v.GetVertexName())
+				return true;
+			else
+				return false;
+		}
+
+		void AddEdge(Edge edge) {
+			if (edge.GetHead() == *this) {
+				int numberOfEdges = length(incidenceList);
+			}
+		}
+};
 
 class Edge {
 	private:
-		int head, tail;
+		int edgeName;
+		Vertex head, tail;
 		float edgeWeight;
+
 	public:
-		void SetEdge(int h, int t, float w) {
-				head = h;
-				tail = t;
+		void SetEdge(Vertex h, Vertex t, float w) {
+				this->head = h;
+				this->tail = t;
 				edgeWeight = w;
 		}
-		int GetEdgeEndpoints(int ht) { // 1 for head, 2 for tails
+		Vertex GetEdgeEndpoints(int ht) { // 1 for head, 2 for tails
 			if (ht == 1) return head;
 			if (ht == 2) return tail;
 		}
@@ -22,12 +68,18 @@ class Edge {
 		}
 		
 		float GetEdgeWeight(void) {
-			return weight;
+			return edgeWeight;
+		}
+
+		Vertex GetHead() {
+
 		}
 };
 
 class SimpleGraph {
 	private:
+
+
 		int numberOfVertices, numberOfEdges; // order and size
 		float *weigthFunction; // weight
 		
@@ -56,3 +108,14 @@ class SimpleGraph {
 		}
 };
 
+
+
+
+
+
+
+
+
+
+
+#endif
